3a2079b9985d22257e3044c8bb7799455d4c1a70
fredeingvardsen <frede.ingvardsen@gmail.com>
2025-10-06 16:22:54 +0200

    delete gm

--- components/offer-ride-form.tsx
+++ components/offer-ride-form.tsx
@@ -9,7 +9,6 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "@/components/ui/textarea"
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group"
-import { getGoogleMapsApiKey } from "@/lib/actions/get-maps-key"
 import { useRouter } from "next/navigation"
 import { useState, useEffect } from "react"
 
